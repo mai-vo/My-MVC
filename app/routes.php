f43c2c1fb5032fffb2b021ef6b32892a1cc80e68
Mai-Vo <mai.vv@neo-lab.vn>
2017-10-06 15:48:03 +0700

    first commit

--- app/routes.php
+++ app/routes.php
@@ -1,14 +1,6 @@
 <?php 
 
-$router->get('','PagesController@home');
-$router->get('about','PagesController@about');
-$router->get('contact','PagesController@contact');
-$router->get('users','UsersController@index');
-$router->post('users','UsersController@store');
-$router->get('users/add','UsersController@add');
-$router->post('users/add','UsersController@store');
-$router->get('users/delete','UsersController@destroy');
-$router->get('users/edit','UsersController@edit');
-$router->post('users/edit','UsersController@update');
+$router->get('admin/users','UsersController@index');
+
 
 ?>
--- app/views/partials/nav.php
+++
@@ -1,7 +0,0 @@
-<ul>
-	<li><a href="" title="">Home</a></li>
-	<li><a href="/about" title="">About</a></li>
-	<li><a href="/about/culture" title="">About our Culture</a></li>
-	<li><a href="/contact" title="">Contact</a></li>	
-	<li><a href="/users" title="">Users</a></li>	
-</ul>
